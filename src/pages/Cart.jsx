48ef7151a78abb57a1ea1b00071663f7a7f417f7
suchithra-sreenivasan <suchithraplathottathil@gmail.com>
2024-10-23 11:50:07 +0530

    e-cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,45 @@
 import { faBackward, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { emptyCart, removeCartItem } from '../redux/slices/cartSlice'
 
 function Cart() {
+
+  const [total, setTotal]=useState(0)
+
+  const dispatch = useDispatch((state)=>state.cartItem)
+  const navigate = useNavigate()
+
+  const cartArray = useSelector((state)=>state.cartItem)
+  console.log(cartArray);
+
+  const getTotal=()=>{
+    if(cartArray.length > 0)
+      {setTotal(cartArray?.map((items)=>items.price).reduce((n1,n2)=>n1+n2))}
+  }
+
+  console.log(total);
+  
+  const handleCheckout= ()=>{
+    alert('order placed successfully')
+    dispatch(emptyCart())
+    navigate('/')
+  }
+
+  useEffect(()=>{
+    getTotal()
+  },[cartArray])
+  
   return (
     <>
     <div className='pt-32'>
       <h1 className='text-center text-4xl text-violet-900'>Cart</h1>
+      {cartArray.length > 0 ?
       <div className='md:grid grid-cols-[2fr_1fr] my-10'>
-        <div className=' md:py-5 md:px-20'>
+        
+          <div className=' md:py-5 md:px-20'>
           <table className='w-full border border-gray-400'>
             <thead>
               <tr>
@@ -21,29 +51,34 @@ function Cart() {
               </tr>
             </thead>
             <tbody>
+             {
+             cartArray.map((items , index)=>(
               <tr>
-                <td className='border border-gray-100 p-3 '>1</td>
-                <td className='border border-gray-100 p-3'>dummy</td>
-                <td className='border border-gray-100 p-3 flex justify-center'><img src="https://aqualogica.in/cdn/shop/products/7__1200x1200__2.jpg?v=1701149493" alt="no image" style={{width:'100px',height:'100px'}} /></td>
-                <td className='border border-gray-100 p-3'>$40</td>
-                <td className='border border-gray-100 p-3 text-center'><button className='bg-red-700  p-3 text-white rounded'><FontAwesomeIcon icon={faTrash} /></button></td>
-              </tr>
+              <td className='border border-gray-100 p-3 '>{index+1}</td>
+              <td className='border border-gray-100 p-3'>{items?.title}</td>
+              <td className='border border-gray-100 p-3 flex justify-center'><img src={items?.image} alt="no image" style={{width:'100px',height:'100px'}} /></td>
+              <td className='border border-gray-100 p-3'>${items?.price}</td>
+              <td className='border border-gray-100 p-3 text-center'>
+                <button onClick={()=>dispatch(removeCartItem(items?.id))} className='bg-red-700  p-3 text-white rounded'><FontAwesomeIcon icon={faTrash} /></button></td>
+            </tr>
+             )) }
             </tbody>
           </table>
         </div>
+        
         <div className='pt-5 px-3'>
           <div className='p-3 shadow-lg'>
             <h1 className='text-center text-3xl'>Cart Summary</h1>
-            <p className='mt-4 text-xl'>Total number of products : 7</p>
-            <p className='mt-2 text-xl'>Grand Total: $700</p>
-            <button className='w-full bg-green-600 text-white p-3 mt-4 hover:bg-white hover:border hover:border-green-600 hover:text-green-600'>Check Out</button>
+            <p className='mt-4 text-xl'>Total number of products : {cartArray?.length}</p>
+            <p className='mt-2 text-xl'>Grand Total: ${total}</p>
+            <button onClick={handleCheckout} className='w-full bg-green-600 text-white p-3 mt-4 hover:bg-white hover:border hover:border-green-600 hover:text-green-600'>Check Out</button>
 
           </div>
         </div>
       </div>
 
 
-      
+      :
       <div className='w-full mt-10 md:grid grid-cols-3'>
         <div></div>
         <div className='flex justify-center items-center flex-col my-10'>
@@ -53,6 +88,7 @@ function Cart() {
         </div>
         <div></div>
       </div>
+}
     </div>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,17 @@ import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import useFetch from '../hooks/useFetch'
+import { useDispatch } from 'react-redux'
+import { addWishlistItem } from '../redux/slices/wishlistSlice'
+import { addItemToCart } from '../redux/slices/cartSlice'
 
 function Home() {
 
   const data = useFetch('https://fakestoreapi.com/products')
   console.log(data);
+
+  const dispatch = useDispatch()
+
   
   return (
     <div className='pt-40 mb-10 px-10 md:grid gap-10 grid-cols-4'>
@@ -20,8 +26,8 @@ function Home() {
           <p className='text-justify'>{items?.description.slice(0,100)}</p>
           <p className='text-2xl'>Price: <span className='text-violet-900'>{items?.price}</span></p>
           <div className='flex justify-between'>
-            <button className='p-3 bg-red-700 text-white'><FontAwesomeIcon icon={faHeart} /></button>
-            <button className=' p-3 bg-green-700 text-white'><FontAwesomeIcon icon={faCartShopping} /></button>
+            <button onClick={()=>dispatch(addWishlistItem(items))} className='p-3 bg-red-700 text-white'><FontAwesomeIcon icon={faHeart} /></button>
+            <button onClick={()=>dispatch(addItemToCart(items))} className=' p-3 bg-green-700 text-white'><FontAwesomeIcon icon={faCartShopping} /></button>
           </div>
         </div>
       </div>
